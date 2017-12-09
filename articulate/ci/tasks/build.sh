183c2411be028e6a4ab9761693ef563721d18215
Bright Zheng <bright.zheng@outlook.com>
2017-12-09 17:41:19 +0800

    followed Maven naming convention

--- articulate/ci/tasks/build.sh
+++ articulate/ci/tasks/build.sh
@@ -1,30 +1,35 @@
 #!/usr/bin/env bash
 set -e
 
-echo "start building artifacts..."
+echo "start building artifacts by Maven..."
 
 # Use a dedicated folder outside the build folder so reuse becomes possible
 
 M2_HOME=${HOME}/.m2
-M2_LOCAL_REPO=${M2_HOME}/repository
-mkdir -p ${M2_LOCAL_REPO}
+mkdir -p ${M2_HOME}
 
-
-# Construct the Maven settings.xml
+M2_LOCAL_REPO="$( pwd )/.m2"
+mkdir -p "${M2_LOCAL_REPO}/repository"
 
 cat > ${M2_HOME}/settings.xml <<EOF
+
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                           https://maven.apache.org/xsd/settings-1.0.0.xsd">
-      <localRepository>${M2_LOCAL_REPO}</localRepository>
+      <localRepository>${M2_LOCAL_REPO}/repository</localRepository>
 </settings>
+
 EOF
 
 
 # Maven build
+mvn --version
 mvn package -f resource-git/articulate/pom.xml
 
 # Copy it to output folder
 cp resource-git/articulate/target/articulate-*.jar artifacts/
 ls -lat artifacts/
+
+
+
