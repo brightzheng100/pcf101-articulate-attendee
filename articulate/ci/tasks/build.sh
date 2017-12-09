4a484413cba487f7c38e57d5ed911dc810ab549a
Bright Zheng <bright.zheng@outlook.com>
2017-12-09 17:27:36 +0800

    followed Maven naming convention

--- articulate/ci/tasks/build.sh
+++ articulate/ci/tasks/build.sh
@@ -5,19 +5,19 @@ echo "start building artifacts..."
 
 # Use a dedicated folder outside the build folder so reuse becomes possible
 
-MAVEN_HOME=${HOME}/.m2
-MAVEN_REPO=${HOME}/.m2/repository
-mkdir -p ${MAVEN_REPO}
+M2_HOME=${HOME}/.m2
+M2_LOCAL_REPO=${M2_HOME}/repository
+mkdir -p ${M2_LOCAL_REPO}
 
 
 # Construct the Maven settings.xml
 
-cat > ${MAVEN_HOME}/settings.xml <<EOF
+cat > ${M2_HOME}/settings.xml <<EOF
 <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0
                           https://maven.apache.org/xsd/settings-1.0.0.xsd">
-      <localRepository>${MAVEN_REPO}</localRepository>
+      <localRepository>${M2_LOCAL_REPO}</localRepository>
 </settings>
 EOF
 
