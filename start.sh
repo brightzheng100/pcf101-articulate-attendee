f5c251c67a5aa7039ddf3a9587a28a8adee6d313
Bright Zheng <bright.zheng@outlook.com>
2018-01-15 17:49:23 +0800

    simplied the start scripts

--- start.sh
+++ start.sh
@@ -14,16 +14,19 @@ echo "=====> ./mvnw package"
 echo "=====> cf push -f attendee/manifest.yml"
 cf push -f attendee/manifest.yml
 
-echo "=====> cf push -f articulate/manifest.yml"
-cf push -f articulate/manifest.yml
-
 echo "=====> cf cups pcf101-demo-attendee-service -p uri https://pcf101-demo-attendee.${DOMAIN}/attendees"
 cf cups pcf101-demo-attendee-service -p "{\"uri\":\"https://pcf101-demo-attendee.${DOMAIN}/attendees\"}"
 
-echo "=====> cf cf bind-service pcf101-demo-articulate pcf101-demo-attendee-service"
-cf bind-service pcf101-demo-articulate pcf101-demo-attendee-service
+#cf push -f articulate/manifest.yml
+cf push -f articulate/manifest-with-service.yml
 
-echo "=====> cf restage pcf101-demo-articulate"
-cf restage pcf101-demo-articulate
+#echo "=====> cf cups pcf101-demo-attendee-service -p uri https://pcf101-demo-attendee.${DOMAIN}/attendees"
+#cf cups pcf101-demo-attendee-service -p "{\"uri\":\"https://pcf101-demo-attendee.${DOMAIN}/attendees\"}"
+
+#echo "=====> cf cf bind-service pcf101-demo-articulate pcf101-demo-attendee-service"
+#cf bind-service pcf101-demo-articulate pcf101-demo-attendee-service
+
+#echo "=====> cf restage pcf101-demo-articulate"
+#cf restage pcf101-demo-articulate
 
 open "https://pcf101-demo-articulate.${DOMAIN}"
